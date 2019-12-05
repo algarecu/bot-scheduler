9e822cdd842bf3ce2f1b0963a36f3456c7e27292
Alvaro Garcia <1146507+algarecu@users.noreply.github.com>
2019-12-05 20:11:01 +0100

    Update bot.sh

--- tasks/bot.sh
+++ tasks/bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Scheduling a single bot now...'
-BOT_RUN=$HOME/Documents/github/com.geodb.location.bot/
+BOT_RUN=$HOME/Documents/github/bot/
 cd $BOT_RUN
 ./environment/master_single_to_sql.sh
